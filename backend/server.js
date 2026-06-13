dfddf5b5c613afc973919eebadb52a6ae9afdf31
Lea-Babara <yuyarbongkem@gmail.com>
2026-06-13 16:58:32 +0100

    port change

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,6 @@ app.use('/api/reservations', reservationsRoutes);
 app.use('/api/finance', financeRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
